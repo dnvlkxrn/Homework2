fc135fab418eb9864ee9176842c0e7854f192b3e
Danila Kornev <danilakornev92@gmail.com>
2023-03-19 21:36:58 +0300

    button action

--- Homework2/ViewController.swift
+++ Homework2/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var pushButton: UIButton!
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pushButton.setTitle("Start", for: .normal)
+        redLight.layer.cornerRadius = 0.5 * redLight.layer.frame.width
+        yellowLight.layer.cornerRadius = redLight.layer.cornerRadius
+        greenLight.layer.cornerRadius = redLight.layer.cornerRadius
     }
 
 
+    @IBAction func pushButtonDidTapped() {
+        pushButton.setTitle("Next", for: .normal)
+    
+        if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        } else if yellowLight.alpha == 1 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        } else { greenLight.alpha = 0.3
+            redLight.alpha = 1
+        }
+    }
 }
 
